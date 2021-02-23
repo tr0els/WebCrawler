34b9958ebce0f48c4f9cd0fc8028b790e6a8b223
Troels <31724037+tr0els@users.noreply.github.com>
2021-02-23 09:51:04 +0100

    Create Url.cs

---
+++ WebCrawler/Url.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCrawler
+{
+    class Url
+    {
+        public string url;
+        public int level;
+    }
+}
